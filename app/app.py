fb865e7a6c45afe8846874fb7a66bb6f8ce81dfd
djbiega <djbiega@gmail.com>
2020-03-20 19:04:22 -0500

    Cleanup

--- app/app.py
+++ app/app.py
@@ -15,86 +15,139 @@ import pandas as pd
 import numpy as np
 
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__,
+                meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+                external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 cols = ['Item', 'Protein', 'Carbohydrates', 'Fat', 'Fiber', 'Sugar', 'Calories']
 
-app.layout = html.Div(
-    children = [
+app.layout = dbc.Container(
+    [
         html.Div(
-            id='banner',
-            className='banner',
-            children=[
-                html.Div(
-                    id='banner-text',
-                    children=[
-                        html.H4('MyFitnessPal Weekly Dashboard'),
-                        html.H6('To access MyFitnessPal Data, Diary settings must be set to public')
-                    ],
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(html.H2('MyFitnessPal Weekly Dashboard'), width=9),
+                        dbc.Col(html.H2('Image'), width=3)
+                    ]
+                ),
+                dbc.Row(
+                    dbc.Col(html.H6('To access MyFitnessPal Data, Diary settings must be set to public'), width=6)
                 ),
             ],
         ),
         html.Div(
-            children = [
-                dbc.Row([
-                    dbc.Col(
-                        dbc.Input(
-                            placeholder='Enter MFP Username...',
-                            id='mfp-username', 
-                            value='', 
-                            type='text',
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dbc.Input(
+                                placeholder='Enter MFP Username...',
+                                id='mfp-username', 
+                                value='', 
+                                type='text',
+                                ),
+                            width = 3),
+                        dbc.Col(dbc.Button('Submit', id='submit-button')),
+                    ], justify='start'
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dbc.Alert(
+                                'Invalid Username',
+                                id='dbc-validate-username',
+                                color='primary',
+                                dismissable=False,
+                                fade=False,
+                                is_open=False,
                             ),
-                        width = 3),
-                    dbc.Col(dbc.Button('Submit', id='submit-button')),
-                ], justify='start'),
-                dbc.Row([
-                    dbc.Col(
-                        dbc.Alert(
-                            'Invalid Username',
-                            id='dbc-validate-username',
-                            color='primary',
-                            dismissable=False,
-                            fade=False,
-                            is_open=False,
+                        width = 3
+                        )
+                    ], justify='start'
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.Div(
+                                    [
+                                        dcc.Graph(
+                                            id='weekly-pie-chart',
+                                            config={
+                                                'displayModeBar': False,
+                                            },
+                                            figure={}
+                                        )
+                                    ],
+                                ),
+                                html.Div(
+                                    [
+                                        dcc.Graph(
+                                            id='weekly-bar-chart',
+                                            config={
+                                                'displayModeBar': False,
+                                            },
+                                            figure={}
+                                        )
+                                    ],
+                                )
+                            ]
                         ),
-                    width = 3)
-                ], justify='start'),
-            ]),
+                        dbc.Col(
+                            html.Div(
+                                [
+                                    # top food by category go here
+                                ]
+                            ), width=6
+                        )
+                    ]
+                )
+            ]
+        ),
         html.Div(
-            children = [
-                html.H4('See Daily Breakdown'),]),
-        html.Div(
-            id = 'display-div',
-            children = [
-                html.Div(id='test-div',children=[
-                    dcc.Graph(
-                        id='week-at-a-glance',
-                        config={
-                            'displayModeBar': False,
-                        },
-                        figure={}
-                        )]),
-                html.Div([
-                    dcc.Dropdown(
-                        id='date-dropdown',
-                        options=[
-                            {'label': datetime.strftime(datetime.today()-timedelta(i), '%Y-%m-%d'), 
-                            'value': datetime.strftime(datetime.today()-timedelta(i), '%Y-%m-%d')} \
-                                for i in range(7)
+            [
+                dbc.Col(
+                    [
+                        dcc.Graph(
+                            id='week-at-a-glance',
+                            config={
+                                'displayModeBar': False,
+                            },
+                            figure={}
+                        )
+                    ], width=12
+                ),
+                dbc.Col(
+                    [
+                        dcc.Dropdown(
+                            id='date-dropdown',
+                            options=[
+                                {
+                                    'label': datetime.strftime(datetime.today()-timedelta(day), '%Y-%m-%d'), 
+                                    'value': datetime.strftime(datetime.today()-timedelta(day), '%Y-%m-%d')
+                                } for day in range(7)
                             ],
                             multi=True),
-                    DataTable(
-                        id='data-table',
-                        style_data={
-                            'whiteSpace': 'normal',
-                            'height': 'auto'
-                        },
-                        columns=[{'name': i, 'id': i} for i in cols]               
-                    )])
-                ],
-            ),
+                        DataTable(
+                            id='data-table',
+                            style_data={
+                                'whiteSpace': 'normal',
+                                'height': 'auto'
+                            },
+                            columns=[{'name': i, 'id': i} for i in cols]               
+                        )
+                    ], width=12
+                )
+            ],
+        ),
         html.Div(id='hidden-data', style={'display': 'none'}),
-    ])
+    ], style={'height': '700'}
+)
 
 
 @app.callback([Output('dbc-validate-username', 'children'),
@@ -138,7 +191,9 @@ def de_jsonify_data(jsonified_data):
     return datasets, df_list, date_list
 
 @app.callback(
-    Output('week-at-a-glance', 'figure'),
+    [Output('week-at-a-glance', 'figure'),
+    Output('weekly-bar-chart', 'figure'),
+    Output('weekly-pie-chart', 'figure')],
     [Input('hidden-data', 'children')])
 def plot_data(jsonified_data):
     if jsonified_data is None:
@@ -149,54 +204,52 @@ def plot_data(jsonified_data):
     for idx, df in enumerate(df_list):
         if df.empty:
             df_list[idx] = pd.DataFrame([[np.nan,0,0,0,0,0,0]], columns=cols).set_index('Item')
-
-    weekly_nutrition = [
-                    {
+    fig = go.Figure()
+    fig.add_trace(go.Scatter({
                         'x': date_list, 
                         'y': [df_list[i].Protein.sum() for i in range(len(df_list))],
                         'type': 'scatter', 
                         'name': 'Protein',
-                        'line': {'color': 'D44D30'}
-                    },
-                    {
+                        'line': {'color': '#ff6361'}
+                    }))
+    fig.add_trace(go.Scatter({
                         'x': date_list, 
                         'y': [df_list[i].Carbohydrates.sum() for i in range(len(df_list))],
                         'type': 'scatter', 
                         'name': 'Carbohydrates',
-                        'line': {'color': 'D7E321'}
-                    },
-                    {
+                        'line': {'color': '#ffa600'}
+                    }))
+    fig.add_trace(go.Scatter({
                         'x': date_list, 
                         'y': [df_list[i].Fat.sum() for i in range(len(df_list))],
                         'type': 'scatter', 
                         'name': 'Fat',
-                        'line': {'color': '4F28C7'}
-                    },
-                    {
+                        'line': {'color': '#003f5c'}
+                    }))
+    fig.add_trace(go.Scatter({
                         'x': date_list, 
                         'y': [df_list[i].Fiber.sum() for i in range(len(df_list))],
                         'type': 'scatter', 
                         'name': 'Fiber',
-                        'line': {'color': '0C8A26'}
-                    },
-                    {
+                        'line': {'color': '#444e86'}
+                    }))
+    fig.add_trace(go.Scatter({
                         'x': date_list, 
                         'y': [df_list[i].Sugar.sum() for i in range(len(df_list))],
                         'type': 'scatter', 
                         'name': 'Sugar',
-                        'line': {'color': 'D77BDD'}
-                    },
-                    {
+                        'line': {'color': '#dd5182'}
+                    }))
+    fig.add_trace(go.Scatter({
                         'x': date_list, 
                         'y': [df_list[i].Calories.sum() for i in range(len(df_list))],
                         'type': 'scatter', 
                         'name': 'Calories', 
                         'yaxis': 'y2',
-                        'line': {'color':'#000000'}
-                    },
-                ]
-    return {'data': weekly_nutrition,
-            'layout': go.Layout(title='Your Week at a Glance', 
+                        'line': {'color':'#955196'}
+                    }))
+    fig.update_layout(
+        title='Your Week at a Glance', 
                                 yaxis={'title': 'Grams'}, 
                                 yaxis2={'title':'Calories', 
                                         'overlaying': 'y', 
@@ -207,8 +260,60 @@ def plot_data(jsonified_data):
                                         'yanchor': 'top',
                                         'x': 0.5,
                                         'y': 1.1},
-                                paper_bgcolor='rgba(0,0,0,0)',
-                                plot_bgcolor='rgba(0,0,0,0)')}
+                                plot_bgcolor='rgba(0,0,0,0)',
+                                margin={
+                                    't': 25
+                                }
+                                )
+
+    fig2 = go.Figure()
+    fig2.add_trace(go.Bar({
+        'x': date_list,
+        'y': [df_list[i].Protein.sum() for i in range(len(df_list))],
+        'name': 'Protein',
+        'marker_color': '#ff6361'
+    }))
+    fig2.add_trace(go.Bar({
+        'x': date_list,
+        'y': [df_list[i].Carbohydrates.sum() for i in range(len(df_list))],
+        'name': 'Carbohydrates',
+        'marker_color': '#ffa600'
+    }))
+    fig2.add_trace(go.Bar({
+        'x': date_list,
+        'y': [df_list[i].Fat.sum() for i in range(len(df_list))],
+        'name': 'Fat',
+        'marker_color': '#003f5c'
+    }))
+    fig2.update_layout(
+        showlegend=True,
+        height=350,
+        width=600,
+        paper_bgcolor='rgba(0,0,0,0)',
+        margin={'t': 0,
+                'r': 50})
+
+    fig3 = go.Figure()
+    fig3.add_trace(go.Pie(
+        labels=['Protein', 'Carbohydrates', 'Fat'], 
+        values=[
+                np.sum([df_list[i].Protein.sum()*4 for i in range(len(df_list))]),
+                np.sum([df_list[i].Carbohydrates.sum()*4 for i in range(len(df_list))]),    
+                np.sum([df_list[i].Fat.sum()*8 for i in range(len(df_list))])
+        ], 
+        textinfo='label+percent', 
+        insidetextorientation='radial',
+        marker_colors=['#ff6361', '#ffa600', '#003f5c'])
+    )
+    fig3.update_layout(
+        showlegend=False,
+        height=375,
+        width=375,
+        margin={'l': 125, 
+                'r': 0,
+                't': 0,
+                'b': 0})
+    return fig, fig2, fig3
 
 
 @app.callback(
